a84dd72087a222445f902c83fb84ba4a20444bed
Aushi Kumari <118545594+Ayushi391@users.noreply.github.com>
2026-09-07 14:42:46 +0530

    Add files via upload
    Snake Game using Turtle Graphics

---
+++ Snake Game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALLIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 265)
+        self.hideturtle()
+        self.update_score()
+
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALLIGNMENT, font =FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over!", align="center", font =("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score+= 1
+        self.clear()
+        self.update_score()
